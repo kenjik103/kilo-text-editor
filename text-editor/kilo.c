c69316229ddc19bab7315c83a753d25a44605edb
Kenjiro Kanagy <kenjik103@gmail.com>
2024-07-04 16:13:00 -0700

    converted terminal input to raw mode + added some error catching

--- text-editor/kilo.c
+++ text-editor/kilo.c
@@ -1,18 +1,30 @@
+/** includes **/
+
+#include <errno.h>
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
 
+/** data **/
 
-struct termios orig_termios; //original terminal settings
+struct termios orig_termios; //original terminal settings.
+
+/** terminal **/
+
+void die(const char *c) {
+	perror(c);
+	exit(1);
+}
 
 void disableRawMode(){
-	tcsetattr(STDIN_FILENO, TCSAFLUSH,  &orig_termios);
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH,  &orig_termios) == -1) die("tcsetattr");
 }
 
 void enableRawMode(){
-	tcgetattr(STDIN_FILENO, &orig_termios); //read current terminal attributes into struct
+	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1 ) die("tcsetattr"); //read current terminal attributes into struct
 	atexit(disableRawMode);
 
 	struct termios raw = orig_termios;
@@ -39,21 +51,31 @@ void enableRawMode(){
 	//IEXTEN: waits for input then sends that as literal char (ctrl-v)
 	raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN); 	
 
+	raw.c_cc[VMIN] = 0; //minimum bytes of input before read() returns
+	raw.c_cc[VTIME] = 1; //if 100 milliseconds pass, read() returns
+
 	//load in struct. TCSAFLUSH will wait for output to be written to term before setting.
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); 
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr"); 
 }
 
+/** init **/
 
 int main (){
 	enableRawMode();
 
-	char c;
-	while(read(STDIN_FILENO,  &c, 1) == 1 && c != 'q'){//read 1 byte from input into c until there are no bytes left
+	while(1){
+		char c = '\0';
+
+		//read will throw -1 and EAGAIN when it times out
+		if (read(STDIN_FILENO,  &c, 1) == -1 && errno != EAGAIN) die("read"); 
+
 		if (iscntrl(c)){ //if input is control type
 			printf("%d\r\n", c); //print ascii value of control type
 		} else {
 			printf("%d '(%c)'\r\n", c, c); 
 		}
+
+		if (c == 'q') break;
 	}
 	return 0;
 }
