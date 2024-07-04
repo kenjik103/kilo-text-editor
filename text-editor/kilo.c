3724fdf8c0044f217f9c64ffae0d804794aa3c92
Kenjiro Kanagy <kenjik103@gmail.com>
2024-07-03 23:27:35 -0700

    implemented "raw mode"

--- text-editor/kilo.c
+++ text-editor/kilo.c
@@ -1,9 +1,11 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
 
 
-struct termios orig_termios;
+struct termios orig_termios; //original terminal settings
 
 void disableRawMode(){
 	tcsetattr(STDIN_FILENO, TCSAFLUSH,  &orig_termios);
@@ -15,8 +17,28 @@ void enableRawMode(){
 
 	struct termios raw = orig_termios;
 
-	raw.c_cflag &= ~(ECHO); //turn off echo: c_cflag is basically a misc flag
-	
+
+	//note: some of these flags aren't really used and won't have a tangible effect, but whoever designed "raw mode" decided
+	//they should be turned off. I labeled the important ones.
+
+	//i_flag: input flags
+	//IXON: flow control (ctrl-s, ctrl-q)
+	//ICRNL: carriage returns (ctrl-m)
+	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+
+	//oflag: output flag
+	//OPOST: automatically prepends each \n with a carriage return
+	raw.c_oflag &= ~(OPOST);
+
+	raw.c_cflag |= (CS8);
+
+	//c_lflag: misc flags; contains both echo and icanon
+	//ECHO: prints typed keys to terminal
+	//ICANON: Canonical mode. Waits before ENTER is hit before taking input
+	//ISIG: contains SIGINT and SIGSTP, or the program killing commands (ctrl c, ctrl z)
+	//IEXTEN: waits for input then sends that as literal char (ctrl-v)
+	raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN); 	
+
 	//load in struct. TCSAFLUSH will wait for output to be written to term before setting.
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); 
 }
@@ -26,6 +48,12 @@ int main (){
 	enableRawMode();
 
 	char c;
-	while(read(STDIN_FILENO,  &c, 1) == 1 && c != 'q'); //read 1 byte from input into c until there are no bytes left
+	while(read(STDIN_FILENO,  &c, 1) == 1 && c != 'q'){//read 1 byte from input into c until there are no bytes left
+		if (iscntrl(c)){ //if input is control type
+			printf("%d\r\n", c); //print ascii value of control type
+		} else {
+			printf("%d '(%c)'\r\n", c, c); 
+		}
+	}
 	return 0;
 }
