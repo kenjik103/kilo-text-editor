acc3a7f35925c3b1858f8321b8c0bc9799bd4ce6
Kenjiro Kanagy <kenjik103@gmail.com>
2024-07-06 21:15:26 -0700

    added page up page down

--- text-editor/kilo.c
+++ text-editor/kilo.c
@@ -11,8 +11,17 @@
 #include <termios.h>
 
 /** defines **/
-#define CTRL_KEY(k) (k & 0x1f) // bitwise AND operation floors the 5th and 6th bit to zero.
 #define KILO_VERSION "0.0.1"
+#define CTRL_KEY(k) (k & 0x1f) // bitwise AND operation floors the 5th and 6th bit to zero.
+
+enum {
+	ARROW_LEFT = 1000, //dealing with keys as integer values so user cant type out escape commands
+	ARROW_RIGHT,
+	ARROW_UP,
+	ARROW_DOWN,
+	PAGE_UP,
+	PAGE_DOWN
+};
 
 /** data **/
 struct editorConfig {
@@ -74,11 +83,43 @@ void enableRawMode(){
 }
 
 
-char editorReadKey(){
+int editorReadKey(){
 	char c;
 	int nread;
 	while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
 		if (nread == -1 && errno != EAGAIN) die("read");
+	}
+
+	if (c == '\x1b') {
+		char seq[3];
+
+		//if read fails (it times out) c is 1 char long and must just be the escape character
+		if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+		if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
+		if (seq[0] == '[') {
+			if (seq[1] >= '0' && seq[1] <= '9') { //if sequence is digit
+				if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+				if (seq[2] == '~') {
+					switch (seq[1]) {
+						case('5'):
+							return PAGE_UP;
+						case('6'):
+							return PAGE_DOWN;
+					}
+				}
+			} else {
+				switch (seq[1]) { //convert to wasd
+					case 'A': return ARROW_UP;
+      					case 'B': return ARROW_DOWN;
+      					case 'C': return ARROW_RIGHT;
+      					case 'D': return ARROW_LEFT;
+      				}	
+			}
+			return '\x1b';
+		}
+	} else {
+		return c;
 	}
 	return c;
 }
@@ -166,25 +207,33 @@ void editorRefreshScreen() {
 }
 
 /** input **/
-void editorMoveCursor(char key){
+void editorMoveCursor(int key){
 	switch (key) {
-		case 'a':
-			E.cx--;
+		case ARROW_LEFT:
+			if (E.cx != 0) {
+				E.cx--;
+			}
 			break;
-		case 'd':
-			E.cx++;
+		case ARROW_RIGHT:
+			if (E.cx != E.screencols - 1) {
+				E.cx++;
+			}
 			break;
-		case 'w':
-			E.cy--;
+		case ARROW_UP:
+			if (E.cy != 0) {
+				E.cy--;
+			}
 			break;
-		case 's':
-			E.cy++;
+		case ARROW_DOWN:
+			if (E.cy != E.screenrows - 1) {
+				E.cy++;
+			}
 			break;
 	}
 }
 
 void editorProcessKeypress() {
-	char c = editorReadKey();
+	int c = editorReadKey();
 
 	switch (c) {
 		case CTRL_KEY('q'):
@@ -194,11 +243,19 @@ void editorProcessKeypress() {
 
 			exit(0);
 			break;
+		case PAGE_DOWN:
+		case PAGE_UP:
+			{
+				int times = E.screenrows;
+				while (times--)
+					editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+			}
+			break;
 
-		case 'w':
-		case 'a':
-		case 's':
-		case 'd':
+		case ARROW_UP:
+		case ARROW_DOWN:
+		case ARROW_LEFT:
+		case ARROW_RIGHT:
 			editorMoveCursor(c);
 			break;
 	}
